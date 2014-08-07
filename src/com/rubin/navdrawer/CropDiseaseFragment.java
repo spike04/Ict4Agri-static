d94d03b1efe469fbd7314dd2f92daca5900ec141
spike04 <rubinbajracharya@yahoo.com>
2014-08-07 11:42:24 +0545

    About Page completed

---
+++ src/com/rubin/navdrawer/CropDiseaseFragment.java
@@ -0,0 +1,19 @@
+package com.rubin.navdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CropDiseaseFragment extends Fragment {
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_crop, container, false);
+         
+        return rootView;
+    }
+}
